aa06416c271a5e0b69ce99de7bfeb9eb74041776
Abhi04-bit <abhishek107174@gmail.com>
2026-02-10 23:05:48 +0530

    added the all destination and city both search part

--- backend/src/routes/destination.routes.js
+++ backend/src/routes/destination.routes.js
@@ -1,10 +1,24 @@
 const express = require("express");
+
 const router = express.Router();
+router.use((req, res, next) => {
+  console.log("DESTINATION ROUTER HIT:", req.method, req.originalUrl);
+  next();
+});
+
 
 const {
-  getDestination
+  getAllDestinations,
+  getDestinationByCity
 } = require("../controllers/destination.controller");
 
-router.get("/:name", getDestination);
+// GET all destinations
+// GET all destinations (force match)
+router.get("", getAllDestinations);
+router.get("/", getAllDestinations);
+
+
+// GET destination by city
+router.get("/:city", getDestinationByCity);
 
 module.exports = router;
