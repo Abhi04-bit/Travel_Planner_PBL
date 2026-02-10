dd0fc13e21efdfd72f8c9ae4c20340658c40064f
Abhi04-bit <abhishek107174@gmail.com>
2026-02-10 23:53:42 +0530

    Added CRUD (Post PUT Delete)

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,13 +7,18 @@ app.use(express.json());
 
 const {
   getAllDestinations,
-  getDestinationByCity
+  getDestinationByCity,
+  createDestination,
+  updateDestination,
+  deleteDestination
 } = require("./controllers/destination.controller");
 
 // DIRECT ROUTES (no router)
 app.get("/api/destination", getAllDestinations);
 app.get("/api/destination/:city", getDestinationByCity);
-
+app.post("/api/destination", createDestination); // Added POST requests for creating a new destination
+app.put("/api/destination/:id", updateDestination); // Added PUT requests for updating a destination by ID
+app.delete("/api/destination/:id", deleteDestination); // Added DELETE requests for deleting a destination by ID
 app.get("/", (req, res) => {
   res.send("Travel Planner Backend Running");
 });
